3ae7238bdb049ffc22f9255cf97a22da343b0579
TAKAHIRO <guihongmuxia07@gmail.com>
2025-04-24 10:27:02 +0900

    card(5)をcard(20)に変更

--- js/test.js
+++ js/test.js
@@ -9,7 +9,7 @@ document.getElementById("flash-test").addEventListener("click", () => {
         new Card(2),
         new Card(3),
         new Card(4),
-        new Card(5),
+        new Card(25),
     ];
 
     const result = judgeHand(testHand);
