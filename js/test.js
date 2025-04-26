0a6b50051c10ae2b9139c6880400ffdeea2d617e
TAKAHIRO <guihongmuxia07@gmail.com>
2025-04-26 12:29:09 +0900

    DOMContentLoadedを適用

--- js/test.js
+++ js/test.js
@@ -2,7 +2,8 @@ import Card from "./card.js";
 import { judgeHand, isFlush, isStraight, isStraightFlush, isRoyalFlush } from "./judge.js"; //judge要素完成時に引用
 
 // flash-test ボタンが押された時に実行
-document.getElementById("flash-test").addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded",() =>{
+   document.getElementById("flash-test").addEventListener("click", () => {
     // テスト用の手札（例：フラッシュのテスト）
     const testHand = [
         new Card(1),  // スペードA
@@ -20,8 +21,8 @@ document.getElementById("flash-test").addEventListener("click", () => {
 
     // HTML に結果を表示
     document.getElementById("result-area").textContent =result;
-});
-document.getElementById("fourcard-test").addEventListener("click",() =>{
+    });
+    document.getElementById("fourcard-test").addEventListener("click",() =>{
     //テスト用手札
     const testHand =[
         new Card(1), //♠A
@@ -32,24 +33,24 @@ document.getElementById("fourcard-test").addEventListener("click",() =>{
     const result = judgeHand(testHand);
     console.log("役判定:",result);
     document.getElementById("result-area").textContent =result;
-});
+    });
 //straight-testボタンが押されたときに実行
-document.getElementById("straight-test").addEventListener("click",() => {
+    document.getElementById("straight-test").addEventListener("click",() => {
 //テスト用手札
-const testHand=[
+    const testHand=[
     new Card(1),  //1
     new Card(15),  //2
     new Card(3),
     new Card(4),
     new Card(5),
-];
-const result = judgeHand(testHand);
-console.log("役判定:",result);
-document.getElementById("result-area").textContent =result;
-});
+    ];
+    const result = judgeHand(testHand);
+    console.log("役判定:",result);
+    document.getElementById("result-area").textContent =result;
+    });
 
 ////straightflush-testボタンが押されたときに実行
-document.getElementById("straightflush-test").addEventListener("click",() =>{
+    document.getElementById("straightflush-test").addEventListener("click",() =>{
     //テスト用手札
     const testHand=[
         new Card(13),
@@ -64,12 +65,12 @@ document.getElementById("straightflush-test").addEventListener("click",() =>{
 
     console.log("役判定:",result);
     document.getElementById("result-area").textContent =result;
-});
+    });
 
 //royalflush-testボタンが押されたときに実行
-document.getElementById("royalflush-test").addEventListener("click",()=>{
+    document.getElementById("royalflush-test").addEventListener("click",()=>{
     console.log("ボタンが押された"); // ← これを追加！
-    
+
     const testHand =[
         new Card(10),// ♠10
         new Card(11),// ♠11
@@ -84,4 +85,5 @@ document.getElementById("royalflush-test").addEventListener("click",()=>{
 
     console.log("役判定:",result);
     document.getElementById("result-area").textContent =result;
+    });
 });
