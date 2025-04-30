ab6f0e7b0a10c678e29dcc3b262987ff8d36d5b7
TAKAHIRO <guihongmuxia07@gmail.com>
2025-04-30 19:05:47 +0900

    フラッシュ修正02

--- js/card.js
+++ js/card.js
@@ -5,5 +5,8 @@ export default class Card{
 getValue(){
     return(this.index - 1) % 13 +1;
     }
+getSuit(){
+    return Math.floor((this.index -1)/13);
+}
 }
 console.log("card.js が読み込まれました（getValue付き）");
